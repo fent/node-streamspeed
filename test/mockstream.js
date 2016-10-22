ce009b49a0015d8589f13a15bd8cfe5ab64ff238
fent <933490+fent@users.noreply.github.com>
2016-10-22 16:13:08 -0400

    Use sinon to test with fake timers

--- test/mockstream.js
+++ test/mockstream.js
@@ -1,6 +1,10 @@
 var PassThrough = require('stream').PassThrough;
 var util        = require('util');
-var mocknow     = require('./mocknow');
+var sinon       = require('sinon');
+
+var clock;
+before(function() { clock = sinon.useFakeTimers(); });
+after(function() { clock.restore(); });
 
 
 /**
@@ -18,24 +22,24 @@ util.inherits(Mock, PassThrough);
 
 /**
  * Runs the function run n times every interval.
- * Calls callback when finished calling it n times.
  *
  * @param {Number} length
  * @param {Number} n
  * @param {Number} interval
- * @param {Function|Boolean} callback
  */
-Mock.prototype.interval = function(length, n, interval, callback) {
-  callback = callback || this.end.bind(this);
+Mock.prototype.interval = function(length, n, interval) {
+  var callback = this.end.bind(this);
   var self = this;
   var i = 0;
 
   var iid = setInterval(function() {
-    mocknow(interval * ++i);
     self.write(new Buffer(length));
-    if (i === n) {
+    if (++i === n) {
       clearInterval(iid);
       process.nextTick(callback);
+    } else {
+      clock.tick(interval);
     }
   }, interval);
+  clock.tick(interval);
 };
